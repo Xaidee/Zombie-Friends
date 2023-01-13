33513a9178b8318f07e7d9657d6baa0686f7df17
Xaidee <xaideems@gmail.com>
2023-01-13 14:19:00 +1300

    Move entity renderer registry to proper EntityRenderersEvent to help with compatability

--- src/main/java/xaidee/zombiefriends/ZombieFriends.java
+++ src/main/java/xaidee/zombiefriends/ZombieFriends.java
@@ -1,35 +1,27 @@
 package xaidee.zombiefriends;
 
-import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.monster.Zombie;
-import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import xaidee.zombiefriends.client.ZombieVariantRenderer;
 
 
 @Mod(ZombieFriends.MOD_ID)
+@Mod.EventBusSubscriber(modid = ZombieFriends.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ZombieFriends {
 
     public static final String MOD_ID = "zombiefriends";
     public static final Logger LOGGER = LogManager.getLogger();
 
     public ZombieFriends() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        bus.addListener(ZombieFriends::onClientSetup);
     }
 
-    public static void onClientSetup(FMLClientSetupEvent event) {
-        event.enqueueWork(() -> {
-            //noinspection unchecked cast
-            ZombieVariantRenderer.OLD_ZOMBIE_RENDER_FACTORY = (EntityRendererProvider<Zombie>)EntityRenderers.PROVIDERS.get(EntityType.ZOMBIE);
-            EntityRenderers.register(EntityType.ZOMBIE, ZombieVariantRenderer::new);
-        });
+    @SubscribeEvent
+    public static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(EntityType.ZOMBIE, ZombieVariantRenderer::new);
     }
 }
